61e8080c333a684d2f062931f34a5b0c9e3fed1a
NazarKozynets <nazarkozynets030606@gmail.com>
2025-11-14 14:54:46 +0200

    'update: tab bar adapt'

--- u/src/components/layout/tab-bar/TabBar.tsx
+++ u/src/components/layout/tab-bar/TabBar.tsx
@@ -1,22 +1,54 @@
-import {type FC} from 'react';
+import {type FC, useRef, useState} from 'react';
 import {Logo} from "./components/logo/Logo.tsx";
 import {LoginButton, SignupButton} from "./components/buttons/Buttons.tsx";
 import './_tab-bar.scss';
 import {useNavigate} from "react-router-dom";
+import {useWindowSize} from "../../../hooks/useWindowSize.ts";
+import burgerMenu from '@/assets/icons/burger-menu.svg';
+import {useClickOutside} from "../../../hooks/useClickOutside.ts";
 
 interface TabBarProps {
     isAuthenticated: boolean;
 }
 
-export const TabBar: FC<TabBarProps> = ({ isAuthenticated } : TabBarProps) => {
+export const TabBar: FC<TabBarProps> = ({isAuthenticated}: TabBarProps) => {
+    const [isBurgerOpen, setIsBurgerOpen] = useState<boolean>(false);
+
+    const tabBarRef = useRef<HTMLDivElement>(null);
+
+    const {width} = useWindowSize();
+    useClickOutside(tabBarRef, () => {
+       if (isBurgerOpen) setIsBurgerOpen(false);
+    });
+
     const navigate = useNavigate();
 
+    const handleClickBurgerMenu = (path: string) => {
+        setIsBurgerOpen(false);
+        navigate(path);
+    }
+
     return (
-        <div className="tab-bar">
+        <div className="tab-bar" ref={tabBarRef}>
             <Logo onClick={() => navigate('/')}/>
             <div className="tab-bar__controls">
-                <SignupButton onClick={() => navigate('/signup/client')}/>
-                <LoginButton onClick={() => navigate('login')}/>
+                {width > 900 ?
+                    <div className="tab-bar__controls-desktop">
+                        <SignupButton onClick={() => navigate('/signup/client')}/>
+                        <LoginButton onClick={() => navigate('login')}/>
+                    </div> :
+                    <div onClick={() => setIsBurgerOpen(!isBurgerOpen)} className="tab-bar__controls-mobile">
+                        <img src={burgerMenu} alt=''/>
+                    </div>
+                }
+            </div>
+            <div className={`tab-bar__burger ${isBurgerOpen ? 'tab-bar__burger--visible' : ''}`}>
+                <div className="tab-bar__button tab-bar__button--signup" onClick={() => handleClickBurgerMenu('/signup/client')}>
+                    Sign up
+                </div>
+                <div className="tab-bar__button tab-bar__button--login" onClick={() => handleClickBurgerMenu('login')}>
+                    Log in
+                </div>
             </div>
         </div>
     )
