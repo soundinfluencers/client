705d6b77df58314858e0505a997626ccde60c7b8
NazarKozynets <nazarkozynets030606@gmail.com>
2025-11-18 15:13:34 +0200

    "update: started work with auth tab bar"

--- u/src/components/layout/tab-bar/TabBar.tsx
+++ u/src/components/layout/tab-bar/TabBar.tsx
@@ -6,19 +6,18 @@ import {useNavigate} from "react-router-dom";
 import {useWindowSize} from "../../../hooks/useWindowSize.ts";
 import burgerMenu from '@/assets/icons/burger-menu.svg';
 import {useClickOutside} from "../../../hooks/useClickOutside.ts";
+import {useAuth} from "../../../contexts/AuthContext.tsx";
 
-interface TabBarProps {
-    isAuthenticated: boolean;
-}
-
-export const TabBar: FC<TabBarProps> = ({isAuthenticated}: TabBarProps) => {
+export const TabBar: FC = () => {
     const [isBurgerOpen, setIsBurgerOpen] = useState<boolean>(false);
 
     const tabBarRef = useRef<HTMLDivElement>(null);
 
+    const {logout, accessToken} = useAuth();
+
     const {width} = useWindowSize();
     useClickOutside(tabBarRef, () => {
-       if (isBurgerOpen) setIsBurgerOpen(false);
+        if (isBurgerOpen) setIsBurgerOpen(false);
     });
 
     const navigate = useNavigate();
@@ -28,28 +27,49 @@ export const TabBar: FC<TabBarProps> = ({isAuthenticated}: TabBarProps) => {
         navigate(path);
     }
 
+    const isDesktop = width > 900;
+
     return (
         <div className="tab-bar" ref={tabBarRef}>
-            <Logo onClick={() => navigate('/')}/>
-            <div className="tab-bar__controls">
-                {width > 900 ?
-                    <div className="tab-bar__controls-desktop">
-                        <SignupButton onClick={() => navigate('/signup/client')}/>
-                        <LoginButton onClick={() => navigate('login')}/>
-                    </div> :
-                    <div onClick={() => setIsBurgerOpen(!isBurgerOpen)} className="tab-bar__controls-mobile">
-                        <img src={burgerMenu} alt=''/>
-                    </div>
-                }
-            </div>
-            <div className={`tab-bar__burger ${isBurgerOpen ? 'tab-bar__burger--visible' : ''}`}>
-                <div className="tab-bar__button tab-bar__button--signup" onClick={() => handleClickBurgerMenu('/signup/client')}>
+            <Logo onClick={() => navigate('/')} />
+
+            {isDesktop && (
+                <div className="tab-bar__controls-desktop">
+                    {!accessToken ? (
+                        <>
+                            <SignupButton onClick={() => navigate('/signup/client')} />
+                            <LoginButton onClick={() => navigate('/login')} />
+                        </>
+                    ) : (
+                        <>
+                            <button className="tab-bar__logout" onClick={logout}>
+                                Logout
+                            </button>
+                        </>
+                    )}
+                </div>
+            )}
+
+            {!isDesktop && (
+                <div className="tab-bar__controls-mobile" onClick={() => setIsBurgerOpen(true)}>
+                    <img className="tab-bar__burger-toggle" src={burgerMenu} alt="" />
+                </div>
+            )}
+
+            <div className={`tab-bar__menu ${isBurgerOpen ? 'tab-bar__menu--open' : ''}`}>
+                <div
+                    className="tab-bar__menu-item tab-bar__menu-item--signup"
+                    onClick={() => handleClickBurgerMenu('/signup/client')}
+                >
                     Sign up
                 </div>
-                <div className="tab-bar__button tab-bar__button--login" onClick={() => handleClickBurgerMenu('login')}>
+                <div
+                    className="tab-bar__menu-item tab-bar__menu-item--login"
+                    onClick={() => handleClickBurgerMenu('/login')}
+                >
                     Log in
                 </div>
             </div>
         </div>
-    )
+    );
 };
